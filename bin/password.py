6607b14d20793eadb614b9206503cb8b38c8cdf9
dordio <pedro.dordio@gmail.com>
2017-11-19 16:14:28 +0000

    Create password.py

---
+++ bin/password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import random
+
+chars = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 ! # $ % & @ + = - _ < >"
+
+def rdmPass(pwLen):
+    charList= chars.split(" ")
+    i = 0
+    password = ""
+    while i < pwLen:
+        letter = charList[random.randint(0,len(charList) - 1)]
+        if letter != " ":
+            password = password + letter
+            i += 1
+
+    password.strip(" ")
+    print password
+
+#pwLen = raw_input("How many characters: ")
+rdmPass(16)
