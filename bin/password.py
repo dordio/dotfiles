e4c5bf358a1f6507cbe7fb43987d6de7977152be
dordio <pedro.dordio@gmail.com>
2020-04-17 04:25:39 +0100

    Update password.py

--- bin/password.py
+++ bin/password.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import random
+from sys import argv
+
+pname, numchars = argv
 
 chars = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 ! # $ % & @ + = - _ < >"
 
@@ -17,4 +20,4 @@ def rdmPass(pwLen):
     print password
 
 #pwLen = raw_input("How many characters: ")
-rdmPass(16)
+rdmPass(int(argv[1]))
