ad95d07339b290744b4e1e8c04f40d829a214e2b
dordio <pedro.dordio@gmail.com>
2021-12-27 17:00:30 +0000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,6 @@ set PATH "$HOME/bin:$HOME/github:$HOME/.local/bin:$PATH"
 
 set fish_greeting
 
-
 alias cbonsai='~/github/cbonsai/cbonsai'
 alias ls='ls -hN --color --group-directories-first'
 alias la='ls -A'
@@ -19,6 +18,8 @@ alias nano='nano -m -g -c -i -y'
 alias fortune='fortune | cowsay'
 alias speedtest='speedtest --simple'
 alias uptime='uptime -p | sed -e "s/up //g"'
+alias weather='curl wttr.in/\?2Fn'
+alias vivaldi='viv.sh'
 
 alias tdu="tmux resize-pane -U $1"
 alias tdd="tmux resize-pane -D $1"
@@ -28,7 +29,6 @@ alias ta="tmux a -t $1"
 alias tn="tmux new -s $1"
 alias tk="tmux kill-session -t $1"
 alias tl="tmux ls"
-
 
 function sudo --description "replacement for 'sudo !!'"
 	if test "$argv" = !!
@@ -46,5 +46,4 @@ function avail --description "shows home free space"
 	df -h /home | grep /dev | awk '{print $4}'
 end
 
-
-neofetch
+fm6000 -r -c random
